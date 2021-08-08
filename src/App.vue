9431ae4feaaed5a6faf2b8f96a46ce2b9b839244
songnumber1 <songnumber1@naver.com>
2021-08-08 21:14:07 +0900

    vuetify install

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <br><br>
+      <v-btn>vuetify Button</v-btn>
     </div>
     <router-view/>
   </div>
